5d1a4d322df9539af9d8593240e4d408e8029ea1
colinamiguel <miguelcolina@correo.unimet.edu.ve>
2023-08-17 06:30:07 -0400

    Cart functionality added. CSS Fixed

--- index.js
+++ index.js
@@ -1,39 +1,171 @@
-//import { getDocs, collection } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js';
-//import { db } from "./firebase";
+import { getDocs, collection } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js';
+import { db } from "./firebase.js";
 
 const productsButton = document.getElementById('nav-bar-products');
 const homeButton = document.getElementById('home-button');
 const aboutButton = document.getElementById('about-button');
 const shopNowButton = document.querySelector('.modal-button');
 const moreProductsButton = document.querySelector('.more-products');
+const featuredGrid = document.querySelector('.featured-grid');
+const cartButton = document.querySelector('.cart');
+const cartGrid = document.querySelector('.cart-grid');
+const closeButton = document.querySelector('.close-button');
+const cartTotal = document.querySelector('.cart-total');
+const cart = document.querySelector('.side-cart');
+const cartNumber = document.querySelector('.number-of-items')
 
 productsButton.addEventListener('click', ()=>{navigateToPage('products')});
 homeButton.addEventListener('click', ()=>{navigateToPage('index')});
 aboutButton.addEventListener('click', ()=>{navigateToPage('about')});
-shopNowButton.addEventListener('click', ()=>{navigateToPage('products')})
-moreProductsButton.addEventListener('click', ()=>{navigateToPage('products')})
+shopNowButton.addEventListener('click', ()=>{navigateToPage('products')});
+moreProductsButton.addEventListener('click', ()=>{navigateToPage('products')});
+cartButton.addEventListener('click',()=>{showCart(true)});
+closeButton.addEventListener('click',()=>{showCart(false)});
 
-//colRef = collection(db, 'products');
-
-console.log('mierds')
+const colRef = collection(db, 'featured');
 
 let featured = [];
-/*
-getDocs(colRef)
-.then((response)=>{
-    response.docs.forEach((document) => {
-        featured.push({...document.data, id: document.id})        
+let cartItems = [];
+let total = 0;
+
+async function getData(){
+    await getDocs(colRef)
+    .then((response)=>{
+        response.forEach(document => {
+            featured.push({...document.data(), id: document.id});
+            console.log(featured)
+        });
     });
-    updateFeatured();
-    console.log(featured);
-});
+    updateFeatured(featured);
+};
 
-*/
+getData();
 
-function updateFeatured(){
-    console.log('mierda');
+function updateCartNumber(type){
+    let value = Number(cartNumber.innerHTML);
+    if(type){
+        if(value === 0){
+            cartNumber.style.display = 'flex';
+            cartNumber.style.textAlign = 'center';
+            cartNumber.style.justifyContent = 'center';
+            value += 1;
+            cartNumber.innerHTML = value;
+        }else{
+            value += 1;
+            cartNumber.innerHTML = value;
+            cartNumber.style.display = "bock";
+        };
+    }else{
+        if(value === 0){
+            cartNumber.style.display = "none";
+        }else{
+            value -= 1;
+            cartNumber.innerHTML = value;
+        };
+    }
+    
+};
+
+function addItemToCart(itemID){
+    let a = featured.filter((item)=>{if(item.id === itemID){return true}})
+    cartItems.push(a[0]);
+    updateCartNumber(true);
+    updateCart(cartItems);
+    console.log('fdjkl')
+};
+
+
+function updateCart(items){
+    let cartItems = '';
+    total = 0;
+    items.forEach((item)=>{
+        total = total + item.price;
+        cartItems = cartItems + `<div class="cart-item">
+        <img class="cart-image" src="${item.image}">
+        <div class="cart-item-container">
+            <p class="item-name">
+                ${item.name}
+            </p>
+            <p class="item-price">
+                $${item.price}
+            </p>
+            <button value = "${item.id}" class="remove-item">
+                Remove
+            </button>
+        </div>
+        <div class="cart-buttons">
+            <button class="cart-button">
+                +
+            </button>
+            <button class="cart-button">
+                -
+            </button>
+        </div>
+    </div>`;
+    });
+    cartGrid.innerHTML = cartItems;
+    cartTotal.innerHTML = `Total: $${total}`;
+
+    const removeButtonsList = document.querySelectorAll('.remove-item');
+    removeButtonsList.forEach((button)=>{
+        button.addEventListener('click',()=>{removeItem(button.value)});
+    });
+};
+
+function updateFeatured(data){
+    let gridElements = '';
+
+    data.forEach((product)=>{
+        gridElements = gridElements + `
+        <div class="featured-product">
+                    <div class="image-container-featured">
+                        <div class="featured-modal">
+                            <button value="${product.id}" class="add-cart-featured">
+                                <span class="material-symbols-outlined">add_shopping_cart</span>
+                            </button>
+                            <button class="search-button-featured">
+                                <span class="material-symbols-outlined">search</span>
+                            </button>
+                        </div>
+                        <img class="featured-image" src="${product.image}">
+                    </div>
+                    <div class="bottom-container-featured">
+                        <p class="featured-name">
+                            ${product.name}
+                        </p>
+                        <p class="featured-price">
+                            $${product.price}
+                        </p>
+                    </div>
+                </div>`
+    });
+
+    featuredGrid.innerHTML = gridElements;
+
+    const addToCartButtons = document.querySelectorAll('.add-cart-featured');
+    addToCartButtons.forEach((button)=>{
+        button.addEventListener('click', ()=>{addItemToCart(button.value)});
+    });
+};
+
+function showCart(value){
+    if(value){
+        cart.style.display = 'flex';
+    }else{
+        cart.style.display = 'none';
+    };
 };
 
 function navigateToPage(string){
     window.location.href = `/${string}.html`;
+};
+
+function removeItem(id){
+    cartItems = cartItems.filter((item)=>{
+        if(item.id !== id){
+            return true;
+        };
+    });
+    updateCart(cartItems);
+    updateCartNumber(false);
 };
